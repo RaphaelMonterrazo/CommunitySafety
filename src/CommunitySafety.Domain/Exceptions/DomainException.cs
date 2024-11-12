489853d026ce648f73bda39e821ae3b05e505c72
Raphael Monterrazo <raphael_monterrazo@hotmail.com>
2024-11-12 19:37:45 -0300

    Created Domain Exception

---
+++ src/CommunitySafety.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,19 @@
+﻿
+using CommunitySafety.Domain.Common;
+
+namespace CommunitySafety.Domain.Exceptions;
+
+public sealed class DomainException : Exception
+{
+    public string ErrorCode { get; private set; }
+
+    public DomainException(Error error) : base(error.Message) 
+    {
+        ErrorCode = error.Code;
+    }
+
+    public override string ToString()
+    {
+        return $"Error Code: {ErrorCode}, Message: {Message}";
+    }
+}
