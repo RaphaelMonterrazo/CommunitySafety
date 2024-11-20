7f924a87dd0f2d1f7c3ebd035ef7130857508604
Raphael Monterrazo <raphael_monterrazo@hotmail.com>
2024-11-20 14:15:31 -0300

    Adjustment in CategoryRepository

--- src/CommunitySafety.Domain/Interfaces/ICategoryRepository.cs
+++ src/CommunitySafety.Domain/Interfaces/ICategoryRepository.cs
@@ -1,7 +1,14 @@
 ﻿
-namespace CommunitySafety.Domain.Interfaces
+using CommunitySafety.Domain.Entities;
+
+namespace CommunitySafety.Domain.Interfaces;
+
+public interface ICategoryRepository
 {
-    public interface ICategoryRepository
-    {
-    }
+    Task<IEnumerable<Category>> GetCategoriesAsync();
+    Task<Category?> GetCategoryByIdAsync(int id);
+    Task<Category> CreateAsync(Category incident);
+    Task<Category> UpdateAsync(Category category);
+    Task<Category> RemoveAsync(Category category);
+
 }
---
+++ src/CommunitySafety.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿
+using CommunitySafety.Domain.Entities;
+using CommunitySafety.Domain.Interfaces;
+using CommunitySafety.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommunitySafety.Infrastructure.Repositories;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly ApplicationDbContext _context;
+    public CategoryRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IEnumerable<Category>> GetCategoriesAsync()
+    {
+        return await _context.Categories.ToListAsync();
+    }
+
+    public async Task<Category?> GetCategoryByIdAsync(int id)
+    {
+        return await _context.Categories.FindAsync(id);
+    }
+
+    public async Task<Category> CreateAsync(Category category)
+    {
+        _context.Categories.Add(category);
+        await _context.SaveChangesAsync();
+        return category;
+    }
+
+    public async Task<Category> UpdateAsync(Category category)
+    {
+        _context.Categories.Update(category);
+        await _context.SaveChangesAsync();
+        return category;
+    }
+
+    public async Task<Category> RemoveAsync(Category category)
+    {
+        _context.Categories.Remove(category);
+        await _context.SaveChangesAsync();
+        return category;
+    }
+}
