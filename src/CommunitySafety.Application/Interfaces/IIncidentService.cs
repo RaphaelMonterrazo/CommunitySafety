abec5373f5a745ed7d431979988947fbc97b99cc
Raphael Monterrazo <raphael_monterrazo@hotmail.com>
2024-11-20 15:26:56 -0300

    Added ICategoryService, IIncidentService and ILocationService Interfaces

---
+++ src/CommunitySafety.Application/Interfaces/IIncidentService.cs
@@ -0,0 +1,13 @@
+﻿
+using CommunitySafety.Application.DTOs;
+
+namespace CommunitySafety.Application.Interfaces;
+
+public interface IIncidentService
+{
+    Task<IEnumerable<IncidentDTO>> GetIncidentsAsync();
+    Task<IncidentDTO> GetIncidentByIdAsync(int id);
+    Task AddAsync(IncidentDTO incidentDTO);
+    Task UpdateAsync(IncidentDTO incidentDTO);
+    Task RemoveAsync(IncidentDTO incidentDTO);
+}
---
+++ src/CommunitySafety.Application/Interfaces/ILocationService.cs
@@ -0,0 +1,12 @@
+﻿using CommunitySafety.Application.DTOs;
+
+namespace CommunitySafety.Application.Interfaces;
+
+public interface ILocationService
+{
+    Task<IEnumerable<LocationDTO>> GetLocationsAsync();
+    Task<LocationDTO> GetLocationByIdAsync();
+    Task AddAsync(LocationDTO locationDTO);
+    Task UpdateAsync(LocationDTO locationDTO);
+    Task RemoveAsync(LocationDTO locationDTO);
+}
