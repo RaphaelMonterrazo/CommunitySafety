359e904fe6deb7d911a6100cf7b654ea7baee8cf
Raphael Monterrazo <raphael_monterrazo@hotmail.com>
2024-11-20 09:59:42 -0300

    Created ReportIncident and ViewIncident controller.  Adjustment in Home controller.

---
+++ src/CommunitySafety.WebUI/Controllers/ViewIncidentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommunitySafety.WebUI.Controllers
+{
+    public class ViewIncidentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
