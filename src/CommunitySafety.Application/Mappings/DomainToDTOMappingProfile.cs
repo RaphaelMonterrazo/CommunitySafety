c29b2c193715cf42b2682f8721dd98451cde5bf3
Raphael Monterrazo <raphael_monterrazo@hotmail.com>
2024-11-20 09:58:38 -0300

    Created DomainToDTOMapping

---
+++ src/CommunitySafety.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CommunitySafety.Application.DTOs;
+using CommunitySafety.Domain.Entities;
+
+namespace CommunitySafety.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Incident, IncidentDTO>().ReverseMap();
+            CreateMap<Location, LocationDTO>().ReverseMap();
+        }
+    }
+}
