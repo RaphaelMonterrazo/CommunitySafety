c2860f79463661dfff4842e5102955b78293169f
Raphael Monterrazo <raphael_monterrazo@hotmail.com>
2024-11-26 23:32:37 -0300

    Adjustment in IncidentDTO and locationDTO

--- src/CommunitySafety.Application/DTOs/IncidentDTO.cs
+++ src/CommunitySafety.Application/DTOs/IncidentDTO.cs
@@ -7,4 +7,6 @@ public class IncidentDTO
     public string? Description { get; set; }
     public int? CategoryId { get; set; }
     public int? LocationId { get; set; }
+    public LocationDTO? Location { get; set; }
+    public CategoryDTO? Category { get; set; }
 }
--- src/CommunitySafety.Application/DTOs/LocationDTO.cs
+++ src/CommunitySafety.Application/DTOs/LocationDTO.cs
@@ -4,6 +4,6 @@ namespace CommunitySafety.Application.DTOs;
 public class LocationDTO
 {
     public int? Id { get; set; }
-    public double? Latitude { get; set; }
-    public double? Longitude { get; set; }
+    public string? Latitude { get; set; }
+    public string? Longitude { get; set; }
 }
